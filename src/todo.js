9030658b68fe1cd90ce1cd8929de51de51070dd0
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-10-08 17:40:05 +0530

    url

--- src/todo.js
+++ src/todo.js
@@ -12,7 +12,7 @@ export default function Todo() {
   const [editTitle, setEditTitle] = useState("");
   const [editDescription, setEditDescription] = useState("");
 
-  const apiUrl = "http://localhost:8000/todos";
+  const apiUrl = "https://todo-backend-awza.onrender.com";
 
   const handleSubmit = () => {
     setError("");
